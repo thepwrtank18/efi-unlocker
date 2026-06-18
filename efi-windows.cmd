73dd7231908834d7963ba9d02cbb1facbf4d3978
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2026-06-18 12:12:53 -0400

    Update efi-windows.cmd

--- efi-windows.cmd
+++ efi-windows.cmd
@@ -1,12 +1,17 @@
 @echo off
 setlocal ENABLEEXTENSIONS
 echo.
-echo EFI Unlocker 1.1.0 for VMware
+echo EFI Unlocker 1.2.0 for VMware
 echo =============================
 echo (c) Dave Parsons 2018
 
 echo.
-set KeyName="HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Player"
+set KeyName="HKLM\SOFTWARE\VMware, Inc.\VMware Player"
+REG QUERY %KeyName% /v InstallPath >nul 2>&1
+if %errorlevel% neq 0 (
+    set KeyName="HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Player"
+)
+
 :: delims is a TAB followed by a space
 for /F "tokens=2* delims=	 " %%A in ('REG QUERY %KeyName% /v InstallPath') do set InstallPath=%%B
 echo VMware is installed at: %InstallPath%
@@ -21,41 +26,55 @@ for /F "tokens=1,2,3,4 delims=." %%a in ("%ProductVersion%") do (
    set Build=%%d
 )
 
-:: echo Major: %Major%, Minor: %Minor%, Revision: %Revision%, Build: %Build%
-
-:: Check version is 14+
-if %Major% lss 14 (
-    echo VMware Workatation/Player version 14 or greater required!
-    exit /b
+echo %Major% | findstr /R /C:"[0-9][0-9][Hh][1-2]" >nul
+if %errorlevel% equ 0 (
+    set IsModern=1
+) else (
+    set IsModern=0
+    if %Major% lss 14 (
+        echo VMware Workstation/Player version 14 or greater required!
+        exit /b
+    )
 )
 
 pushd %~dp0
 
 echo.
-echo Patching 32-bit ROM...
-xcopy /F /Y "%InstallPath%x64\EFI32.ROM" .
-.\windows\UEFIPatch.exe EFI32.ROM patches.txt -o EFI32-MACOS.ROM
-del /f EFI32.ROM
-
-echo.
-echo Patching 64-bit ROM...
-xcopy /F /Y "%InstallPath%x64\EFI64.ROM" .
-.\windows\UEFIPatch.exe EFI64.ROM patches.txt -o EFI64-MACOS.ROM
-del /f EFI64.ROM
-
-:: Check version is 17+
-if %Major% geq 17 (
+if exist "%InstallPath%x64\EFI32.ROM" (
+    echo Patching 32-bit ROM...
+    xcopy /F /Y "%InstallPath%x64\EFI32.ROM" .
+    .\windows\UEFIPatch.exe EFI32.ROM patches.txt -o EFI32-MACOS.ROM
+    del /f EFI32.ROM
     echo.
-    echo Patching 32-bit TPM 2.0 ROM...
-    xcopy /F /Y "%InstallPath%x64\EFI20-32.ROM" .
-    .\windows\UEFIPatch.exe EFI20-32.ROM patches.txt -o EFI20-32-MACOS.ROM
-    del /f EFI20-32.ROM
+)
 
-    echo.
-    echo Patching 64-bit TPM 2.0 ROM...
-    xcopy /F /Y "%InstallPath%x64\EFI20-64.ROM" .
-    .\windows\UEFIPatch.exe EFI20-64.ROM patches.txt -o EFI20-64-MACOS.ROM
-    del /f EFI20-64.ROM
+if exist "%InstallPath%x64\EFI64.ROM" (
+    echo Patching 64-bit ROM...
+    xcopy /F /Y "%InstallPath%x64\EFI64.ROM" .
+    .\windows\UEFIPatch.exe EFI64.ROM patches.txt -o EFI64-MACOS.ROM
+    del /f EFI64.ROM
+)
+
+set PatchTPM=0
+if %IsModern% equ 1 set PatchTPM=1
+if %IsModern% equ 0 if %Major% geq 17 set PatchTPM=1
+
+if %PatchTPM% equ 1 (
+    if exist "%InstallPath%x64\EFI20-32.ROM" (
+        echo.
+        echo Patching 32-bit TPM 2.0 ROM...
+        xcopy /F /Y "%InstallPath%x64\EFI20-32.ROM" .
+        .\windows\UEFIPatch.exe EFI20-32.ROM patches.txt -o EFI20-32-MACOS.ROM
+        del /f EFI20-32.ROM
+    )
+
+    if exist "%InstallPath%x64\EFI20-64.ROM" (
+        echo.
+        echo Patching 64-bit TPM 2.0 ROM...
+        xcopy /F /Y "%InstallPath%x64\EFI20-64.ROM" .
+        .\windows\UEFIPatch.exe EFI20-64.ROM patches.txt -o EFI20-64-MACOS.ROM
+        del /f EFI20-64.ROM
+    )
 )
 
 popd
